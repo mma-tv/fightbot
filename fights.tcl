5ff69f0bd46d0d915ac71fcaeec9a026dd18830a
makk <makk.mma@gmail.com>
2012-03-25 02:43:35 -0400

    Bump version number

--- fights.tcl
+++ fights.tcl
@@ -8,7 +8,7 @@
 # Author: makk@EFnet
 #
 # Release Date: May 14, 2010
-#  Last Update: Mar 24, 2012
+#  Last Update: Mar 25, 2012
 #
 # Requirements: Eggdrop 1.6.16+, TCL 8.5+, SQLite 3.6.19+
 #
@@ -39,7 +39,7 @@ variable putCommand      putnow        ;# send function: putnow, putquick, putse
 variable debugLogLevel   8             ;# log all output to this log level [1-8, 0 = disabled]
 
 
-variable scriptVersion "1.4.9"
+variable scriptVersion "1.5.0"
 variable ns [namespace current]
 variable poll
 variable pollTimer
