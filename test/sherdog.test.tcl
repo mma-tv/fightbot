a2b2d8df733e37a612debd51c39c1d6a85e01cb7
makk-mma <makk.mma@gmail.com>
2019-10-31 18:22:55 -0400

    Update tests

--- test/sherdog.test.tcl
+++ test/sherdog.test.tcl
@@ -1,21 +1,27 @@
-#! /usr/bin/env tclsh
+#!/usr/bin/env tclsh
 
 ::tcl::tm::path add [file normalize [file join [file dirname [info script]] ..]]
 
+package require url
 package require sherdog
 package require tcltest
 namespace import ::tcltest::*
 
-test sherdog::query {should fetch fighter data} {
-    if {[sherdog::query "ronda rousey" data err]} {
-        return [expr {[llength $data] > 0}]
-    }
-    return 0
-} 1
+test sherdog::query "should parse fighter data" -setup {
+    set html [url::get "https://www.sherdog.com/fighter/Ronda-Rousey-73073"]
+} -body {
+    set fighter [sherdog::parse $html]
+    dict get $fighter name
+} -result "Ronda Rousey"
 
-test sherdog::query {should return error message for failed query} {
-    if {[sherdog::query "aaaaaaaaaaaaa" data err]} {
-        return 0
-    }
-    return [expr {[string length $err] > 0}]
-} 1
+test sherdog::query "should print fighter data" -body {
+    set ret [sherdog::query "ronda rousey" data err]
+    expr {$ret && [llength $data]}
+} -result 1
+
+test sherdog::query "should return error message for failed query" -body {
+    set ret [sherdog::query "aaaaaaaaaaaaa" data err]
+    expr {!$ret && $err ne ""}
+} -result 1
+
+cleanupTests
