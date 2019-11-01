c9fe96111518cc4b7f75fc0a29b04bfeaaf0be50
makk-mma <makk.mma@gmail.com>
2019-10-31 20:34:32 -0400

    Make test less brittle

--- test/url.test.tcl
+++ test/url.test.tcl
@@ -8,11 +8,11 @@ namespace import ::tcltest::*
 
 test url::get "should return content at URL" -body {
     url::get "https://www.google.com"
-} -match glob -result "*<title>Google</title>*"
+} -match glob -result "*<title>*Google*</title>*"
 
 test url::get "should follow redirects" -body {
     # the following should redirect to https://www.google.com
     url::get "http://google.com"
-} -match glob -result "*<title>Google</title>*"
+} -match glob -result "*<title>*Google*</title>*"
 
 cleanupTests
